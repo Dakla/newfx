8c7f055dc9a12ab625429375ab7aa5c05a717948
dakla <dakla2013@gmail.com>
2020-02-18 07:42:52 +0800

    ads

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
         primaryStage.setTitle("Test");
         primaryStage.setScene(new Scene(root, 717, 443));
         primaryStage.show();
+
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             session.getTransaction().commit();
